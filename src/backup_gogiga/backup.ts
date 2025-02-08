b024219feda93f748748a4cfd64ed686150f78aa
piotr7109 <piotrkrk83@gmail.com>
2025-02-08 11:50:45 +0100

    change backup dir

--- src/backup_gogiga/backup.ts
+++ src/backup_gogiga/backup.ts
@@ -6,17 +6,17 @@ export const backupGogiga = async () => {
   await mongoose.connect(process.env.GOGIGA_MONGODB!);
 
   const backupName = `backup_${new Date().getTime()}`;
-  mkdirSync(__dirname + "/" + backupName);
+
+  const dir = __dirname + "/../../backups_gogiga/" + backupName;
+
+  mkdirSync(dir);
   for (const model of MODELS) {
     const data =
       model.modelName === "User"
         ? //@ts-ignore
           await model.find().select("+password").exec()
         : await model.find();
-    writeFileSync(
-      __dirname + `/${backupName}/${model.modelName}.json`,
-      JSON.stringify(data)
-    );
+    writeFileSync(`${dir}/${model.modelName}.json`, JSON.stringify(data));
   }
 
   mongoose.connection.close();
